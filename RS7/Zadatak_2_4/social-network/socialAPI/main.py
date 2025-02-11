0f0d7f82dedd318bd8c5943ecf91b864cdbcaf13
dzagorjan <dzagorjan55@gmail.com>
2025-02-11 17:54:38 +0100

    Docker Compose
    Docker Compose

---
+++ RS7/Zadatak_2_4/social-network/socialAPI/main.py
@@ -0,0 +1,42 @@
+from typing import List
+from fastapi import FastAPI, HTTPException, requests
+from models import ObjavaResponse, ObjavaRequest, LoginRequest
+import datetime
+
+
+app = FastAPI()
+
+objave = [{"id": 1, "korisnik": "marko123", "tekst": "Danas je predivan dan!", "vrijeme": "2025-02-08T10:30:00Z"},
+        {"id": 2, "korisnik": "ana_m", "tekst": "Upravo sam završila čitanje sjajne knjige!", "vrijeme": "2025-02-08T11:15:30Z"},
+        {"id": 3, "korisnik": "petarP", "tekst": "Oduševljen sam novim filmom!", "vrijeme": "2025-02-08T12:05:20Z"},
+        {"id": 4, "korisnik": "luka_99", "tekst": "Pogledajte moj novi crtež!", "vrijeme": "2025-02-08T13:40:10Z"},
+        {"id": 5, "korisnik": "ivaS", "tekst": "Konačno završila projekt na kojem sam radila!", "vrijeme": "2025-02-08T14:25:50Z"}
+    ]
+
+
+@app.post("/objava", response_model=ObjavaResponse)
+def dodaj_objavu(nova_objava:ObjavaRequest):
+    new_id = len(objave)+1
+    objava = ObjavaResponse(id=new_id,korisnik=nova_objava.korisnik, tekst=nova_objava.tekst, vrijeme=datetime.datetime.now())
+    objave.append(objava)
+    return objava
+
+
+
+@app.get("/objava/{id}", response_model=ObjavaResponse)
+def dohvati_objavu(id:int):
+    for objava in objave:
+        if objava["id"] == id:
+            return objava
+    raise HTTPException(status_code=404, detail="Nije pronađena tražena objava.")
+
+
+@app.get("/korisnici/{korisnik}/objave", response_model=List[ObjavaResponse])
+def dohvati_objavu_korisnik(request:LoginRequest):
+
+    response = requests.get("http://authapi:9000/login", json=request.model_dump())
+    if response.status_code !=200:
+        raise HTTPException(status_code=400, detail="Neispravni podaci")
+
+    return [objava for objava in objave if objava['korisnik'] == request.korisnik]
+    
---
+++ RS7/Zadatak_2_4/social-network/socialAPI/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+class ObjavaRequest(BaseModel):
+    korisnik: str = Field(max_length=20)
+    tekst: str = Field(max_length=280)
+    vrijeme: datetime
+
+class ObjavaResponse(ObjavaRequest):
+    id: int
+
+class LoginRequest(BaseModel):
+    korisnik: str
+    lozinka: str
